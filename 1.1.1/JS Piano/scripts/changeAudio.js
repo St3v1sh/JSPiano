89f6ff74979020931d0c6313ff80ea023470394f
st3v1sh <st3v1sh@gmail.com>
2026-02-05 00:04:10 -0500

    remove redundant files

--- 1.1.1/JS Piano/scripts/changeAudio.js
+++
@@ -1,8 +0,0 @@
-function changeAudio(piano) {
-    var audioFiles = $("notes").childNodes;
-    
-    var i;
-    for(i = 0; i < audioFiles.length; i++) {
-        audioFiles[i].src = "notes/"+piano+"/notes/"+audioFiles[i].id+".mp3";
-    }
-}
